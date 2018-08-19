24a4914d223f7d02c98455838ae978ef3a309de0
CainLuo <350116542@qq.com>
2018-08-20 00:13:40 +0800

    添加工程文件
    添加工程文件

---
+++ CLSensorManagerExample/CLSensorManager/EXSensor/Controller/ESSensorController.m
@@ -0,0 +1,25 @@
+//
+//  ESSensorController.m
+//  CLSensorManager
+//
+//  Created by Cain Luo on 2018/8/20.
+//  Copyright © 2018年 Cain Luo. All rights reserved.
+//
+
+#import "ESSensorController.h"
+
+@interface ESSensorController ()
+
+@end
+
+@implementation ESSensorController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.title = @"Sensor";
+}
+
+@end
